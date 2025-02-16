54cd949850065b3438474ce878d1de5d55ccd91c
pleasebelieveme <cain1227@naver.com>
2025-02-17 01:11:37 +0900

    feat: Spring Security 필터 단계에서 동작해서, 인증/인가 실패 시점에 바로 예외를 처리

--- src/main/java/com/example/tasteguidebackv2/common/exception/CommonErrorCode.java
+++ src/main/java/com/example/tasteguidebackv2/common/exception/CommonErrorCode.java
@@ -12,7 +12,9 @@ public enum CommonErrorCode implements ErrorCode {
     MISSING_PARAMETER(HttpStatus.BAD_REQUEST, "C002", "필수 파라미터가 누락되었습니다."),
     INVALID_JSON_FORMAT(HttpStatus.BAD_REQUEST, "C003", "요청 형식이 올바르지 않습니다."),
     METHOD_NOT_ALLOWED(HttpStatus.METHOD_NOT_ALLOWED, "C004", "허용되지 않은 HTTP 메서드입니다."),
-    UNSUPPORTED_MEDIA_TYPE(HttpStatus.UNSUPPORTED_MEDIA_TYPE, "C005", "지원하지 않는 미디어 타입입니다."),;
+    UNSUPPORTED_MEDIA_TYPE(HttpStatus.UNSUPPORTED_MEDIA_TYPE, "C005", "지원하지 않는 미디어 타입입니다."),
+    UNAUTHORIZED(HttpStatus.UNAUTHORIZED, "C006", "인증이 필요합니다."),
+    ACCESS_DENIED(HttpStatus.FORBIDDEN, "C007", "접근 권한이 없습니다.");
 
 	private final HttpStatus status;
 	private final String code;
--- src/main/java/com/example/tasteguidebackv2/common/jwt/JwtAccessDeniedHandler.java
+++ src/main/java/com/example/tasteguidebackv2/common/jwt/JwtAccessDeniedHandler.java
@@ -5,28 +5,28 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.core.AuthenticationException;
-import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.web.access.AccessDeniedHandler;
 import org.springframework.stereotype.Component;
 import com.example.tasteguidebackv2.common.exception.CommonErrorCode;
 
 import java.io.IOException;
 
-// JwtAuthenticationEntryPoint가 없으면 인증실패시 시큐리티가 소셜로그인으로 리다이렉트
 @Component
 @RequiredArgsConstructor
-public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint {
+public class JwtAccessDeniedHandler implements AccessDeniedHandler {
 
     private final ObjectMapper objectMapper;
 
+    // 권한 부(로그인은 되어있지만 ROLE_ADMIN만 접근 가능한 페이지에 ROLE_USER가 요청한 경우)
     @Override
-    public void commence(HttpServletRequest request,
-                         HttpServletResponse response,
-                         AuthenticationException authException) throws IOException {
+    public void handle(HttpServletRequest request,
+                       HttpServletResponse response,
+                       AccessDeniedException accessDeniedException) throws IOException {
         response.setContentType("application/json;charset=UTF-8");
-        response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+        response.setStatus(HttpServletResponse.SC_FORBIDDEN);
 
-        ErrorResponse error = ErrorResponse.of(CommonErrorCode.UNAUTHORIZED);
+        ErrorResponse error = ErrorResponse.of(CommonErrorCode.ACCESS_DENIED);
 
         response.getWriter().write(objectMapper.writeValueAsString(error));
     }
